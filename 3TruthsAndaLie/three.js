b6e28c933f1a1ec017d6463a358898354a010208
Vapor-Uni <801448@sylvaniastudents.org>
2025-04-03 11:58:14 -0400

    Started 3taal project

---
+++ 3TruthsAndaLie/three.js
@@ -0,0 +1,14 @@
+
+
+
+  function truthOneInput() {
+      document.write("Nope, this is true! I have hit my head on many things before, but still no brain injuries! <br/> Restart and try again!"); }
+
+  function truthTwoInput() {
+    document.write("Nope, I have been teaching myself guitar for around 4 months now! <br/> Restart and try again!"); }
+
+  function truthThreeInput() {
+    document.write("Nope, this is true! I have hit my head on many things before, but still no brain injuries! <br/> Restart and try again!"); }
+
+  function LieInput() {
+    document.write("Good job! You got the lie"); }
