8c7f8dd36155a460b1e860851cdef828518ec868
Vapor-Uni <801448@sylvaniastudents.org>
2025-04-07 11:51:05 -0400

    java22 base done

--- 3TruthsAndaLie/three.js
+++ 3TruthsAndaLie/three.js
@@ -2,7 +2,7 @@
 
 
   function truthOneInput() {
-      document.write("Nope, this is true! I have hit my head on many things before, but still no brain injuries! <br/> Restart and try again!"); }
+    document.write("Nope, this is true! I have hit my head on many things before, but still no brain injuries! <br/> Restart and try again!"); }
 
   function truthTwoInput() {
     document.write("Nope, I have been teaching myself guitar for around 4 months now! <br/> Restart and try again!"); }
