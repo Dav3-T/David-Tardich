7f57a5e80aaf526a278eb55a577fd44e4f6eec0c
Vapor-Uni <801448@sylvaniastudents.org>
2025-04-11 12:06:06 -0400

    Finished code with bugs

--- SnakeGame/snakegame.js
+++ SnakeGame/snakegame.js
@@ -1,22 +1,27 @@
 const canvas = document.getElementById("snakeCanvas");
-const ctx = canvas.getContent("2d");
+const ctx = canvas.getContext("2d");
 
-const titleSize = 20;
-const gridSize = canvas.width / titleSize
+const tileSize = 20;
+const gridSize = canvas.width / tileSize;
 
-let snake = [{x: 5, y: 5}];
-let food = {x: 10, y: 10}
+let snake = [{ x: 5, y: 5}];
+let food = {x: 10, y: 10};
 let direction = "right";
 
 function drawSnake() {
     ctx.fillStyle = "green";
     snake.forEach(segment => {
-        ctx.fillRect(segment.x * titleSize, segment.y * titleSize, titleSize);
+        ctx.fillRect(segment.x * tileSize, segment.y * tileSize, tileSize, tileSize);
     });
 }
 
+function drawFood() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x * tileSize, food.y * tileSize,  tileSize, tileSize);
+}
+
 function moveSnake() {
-    const head = {... snake[0] }
+    const head = { ...snake[0] }
 
     switch (direction) {
         case "up":
@@ -35,7 +40,7 @@ function moveSnake() {
 
     snake.unshift(head);
 
-    if (head === food.x && head.y === food.y) {
+    if (head.x === food.x && head.y === food.y) {
         generateFood();
     } else {
         snake.pop();
@@ -44,6 +49,57 @@ function moveSnake() {
 
 function generateFood() {
     food = {
-        x: Math.floor(Math.random() * gridSize)
+        x: Math.floor(Math.random() * gridSize),
+        y: Math.floor(Math.random() * gridSize)
+    };
+}
+
+function checkCollisions() {
+    const head = snake[0];
+
+    if (head.x < 0 || head.x >= gridSize || head.y < 0 || head.y >= gridSize) {
+        resetGame();
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+        if (head.x === snake[i].x && head.y === snake[i].y) {
+            resetGame();
+        }
     }
 }
+
+function resetGame() {
+    alert("Game over! Restarting...");
+    snake = [{ x: 5, y: 5}];
+    direction = "right"
+    generateFood();
+}
+
+function updateGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    drawFood();
+    moveSnake();
+    checkCollisions();
+}
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowUp":
+            direction = "up";
+            break;
+        case "ArrowDown":
+            direction = "down";
+            break;
+        case "Arrowleft":
+            direction = "left";
+            break;
+        case "ArrowRight":
+            direction = "right"
+            break;
+    }
+});
+
+setInterval(updateGame, 100)
+
+generateFood();
